35650f53f5f741abaab2fc5014575f76cfff2e7b
youto023 <yuuto9036@gmail.com>
2025-04-24 12:10:20 +0900

    スフィア上昇

--- DirectXGame/Particle.cpp
+++ DirectXGame/Particle.cpp
@@ -1,5 +1,7 @@
 #include "Particle.h"
 
+using namespace MathUtility;
+
 void Particle::Initialize(Model* model) {
 
 	//NULLポインタチェック
@@ -8,17 +10,24 @@ void Particle::Initialize(Model* model) {
 	model_ = model;
 	// ワールド変換の初期化
 	worldTransform_.Initialize();
+	//色の設定
+	objectColor_.Initialize();
+	color_ = {1, 1, 0, 1};
 
 }
 
 void Particle::Update() {
 
-	//行列を定数バッファに転送
-	worldTransform_.TransferMatrix();
+	//移動
+	worldTransform_.translation_ += {0.0f, 0.1f, 0.0f};
+	//行列を更新
+	worldTransform_.UpdateMatrix();
+	//色変更オブジェクトに色の数値を設定する
+	objectColor_.SetColor(color_);
 }
 
 void Particle::Draw(Camera& camera) {
 
 	//3Dモデル描画
-	model_->Draw(worldTransform_, camera);
+	model_->Draw(worldTransform_, camera,&objectColor_);
 }
--- DirectXGame/Particle.h
+++ DirectXGame/Particle.h
@@ -18,5 +18,9 @@ private:
 	WorldTransform worldTransform_;
 	// モデル
 	Model* model_ = nullptr;
+	//色変更オブジェクト
+	ObjectColor objectColor_;
+	//色の数値
+	Vector4 color_;
 
 };
