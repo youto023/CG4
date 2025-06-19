a0085d7b11f0aed0b0557327f7ce368d67dac324
youto023 <yuuto9036@gmail.com>
2025-06-19 11:47:45 +0900

    エラー出たので直した

--- External/KamataEngine/include/3d/WorldTransform.h
+++ External/KamataEngine/include/3d/WorldTransform.h
@@ -54,6 +54,11 @@ public:
 	/// <returns>定数バッファ</returns>
 	const Microsoft::WRL::ComPtr<ID3D12Resource>& GetConstBuffer() const { return constBuffer_; }
 
+	/// <summary>
+	/// 行列を更新する
+	/// </summary>
+	void UpdateMatrix();
+
 private:
 	// 定数バッファ
 	Microsoft::WRL::ComPtr<ID3D12Resource> constBuffer_;
